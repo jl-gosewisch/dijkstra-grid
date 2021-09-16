c738bba143180e77860b4c8eb6cd895bc64daab9
jl-gosewisch <54152904+jl-gosewisch@users.noreply.github.com>
2021-09-16 18:49:32 +0200

    padding on small viewport

--- components/Button.js
+++ components/Button.js
@@ -6,7 +6,7 @@ export default function ControllButton({handler, text}) {
         <motion.button
         whileHover={{scale:1.1}}
         whileTap={{scale: .9}}
-        type="button" className="rounded-md ml-1 mr-1 sm:ml-2 sm:mr-2 sm:p-2 text-sm sm:text-md text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-red-500 hover:to-yellow-400" onClick={() => handler()}>
+        type="button" className="rounded-md ml-1 mr-1 sm:ml-2 sm:mr-2 p-1 md:p-3 text-sm sm:text-md text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-red-500 hover:to-yellow-400" onClick={() => handler()}>
             {text}
         </motion.button>
     )
